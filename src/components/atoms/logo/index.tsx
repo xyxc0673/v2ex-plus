7f24761c4f300337ef020faf6428036d74e737d1
xyxc0673 <xyxc0673@gmail.com>
2021-02-21 11:20:06 +0800

    feat: add logo component

---
+++ src/components/atoms/logo/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+
+interface IProps {
+  width?: number | string;
+  height?: number | string;
+  resizeMode?: 'contain' | 'cover' | 'stretch' | 'repeat' | 'center';
+}
+
+const Logo = ({ width = 42, height = 24, resizeMode = 'contain' }: IProps) => {
+  return (
+    <View style={{ width, height }}>
+      <Image
+        style={styles.logo}
+        resizeMode={resizeMode}
+        source={require('../../../assets/images/logo.png')}
+      />
+    </View>
+  );
+};
+
+export default Logo;
+
+const styles = StyleSheet.create({
+  logo: {
+    width: '100%',
+    height: '100%',
+  },
+});
