773bb1f890e009b9d3090f89196170034576e8d3
xyxc0673 <xyxc0673@gmail.com>
2021-02-21 10:30:48 +0800

    feat: add default theme and colors

--- src/navigations/application.tsx
+++ src/navigations/application.tsx
@@ -4,13 +4,14 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Tabs } from './index';
 import { StyleSheet } from 'react-native';
+import { defaultTheme } from '@/theme/colors';
 
 const Stack = createStackNavigator();
 
 const ApplicationNavigations = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <NavigationContainer>
+      <NavigationContainer theme={defaultTheme}>
         <Stack.Navigator initialRouteName="TabNavigator">
           <Stack.Screen name="TabNavigator" component={Tabs} />
         </Stack.Navigator>
---
+++ src/theme/colors.ts
@@ -0,0 +1,16 @@
+import { DefaultTheme } from '@react-navigation/native';
+
+export const defaultTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    white: '#fffff',
+    black: '#000000',
+    primary: '#f2994a',
+    secondary: '#363433',
+    secondaryText: '737C79',
+    grey: 'D8D8D8',
+    lightGrey: 'F5F6FA',
+    blue: '0070C9',
+  },
+};
