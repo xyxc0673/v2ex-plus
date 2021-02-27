020349353fd96610ea3e74009bc3095e47644ea7
xyxc0673 <xyxc0673@gmail.com>
2021-02-27 09:41:41 +0800

    chore: add eslint rules for react native

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,18 @@
 module.exports = {
   root: true,
-  extends: ['@react-native-community', 'prettier'],
-  rules:{
-    "prettier/prettier": ["error", {
-     "endOfLine":"auto"
-   }],
-  }
+  extends: ['@react-native-community', 'prettier', 'react', 'react-native'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    "react-native/no-unused-styles": 2,
+    "react-native/split-platform-components": 2,
+    "react-native/no-inline-styles": 2,
+    "react-native/no-color-literals": 2,
+    "react-native/no-raw-text": 2,
+    "react-native/no-single-element-style-arrays": 2,
+  },
 };
