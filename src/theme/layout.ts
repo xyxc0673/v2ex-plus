8bf508170e03ff01f2e716b50f4cd9ffe10f0652
xyxc0673 <xyxc0673@gmail.com>
2021-02-27 19:51:58 +0800

    feat: add Layout

---
+++ src/theme/layout.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+const Layout = StyleSheet.create({
+  column: {
+    flexDirection: 'column',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  fill: {
+    width: '100%',
+    height: '100%',
+  },
+  fullWidth: {
+    width: '100%',
+  },
+  fullHeight: {
+    height: '100%',
+  },
+});
+
+export default Layout;
