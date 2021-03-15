bda896240c620ead7d64e1146606561ee1d4f44e
xyxc0673 <xyxc0673@gmail.com>
2021-03-15 20:31:42 +0800

    feat: add parseTopicList

--- src/services/crawler/parser.ts
+++ src/services/crawler/parser.ts
@@ -1,3 +1,4 @@
+import { ITopic } from '@/interfaces/topic';
 import cheerio from 'cheerio';
 
 export const getTopicId = (link?: string): number => {
@@ -25,4 +26,54 @@ export const getUserInfo = (html: string) => {
 
 export const parseDatetime = (datetime: string) => {
   return datetime.replace('+08:00', '').trim();
+};
+
+export const parseTopicList = (
+  $: cheerio.Root,
+  topicListHtml: cheerio.Cheerio,
+) => {
+  const topicList = [] as Array<ITopic>;
+
+  topicListHtml.each((_, elem) => {
+    const selector = $(elem);
+
+    const title = selector.find('.topic-link').text();
+    const titleHref = selector.find('.topic-link').attr('href');
+    const topicId = getTopicId(titleHref);
+
+    const replyCount =
+      selector.find('.count_livid').text() ||
+      selector.find('.count_orange').text() ||
+      '0';
+
+    const avatar = selector.find('.avatar').attr('src') || '';
+    const topicInfo = selector.find('.topic_info');
+    const vote = $(topicInfo.find('.votes')).text() || '0';
+
+    const node = $(topicInfo.find('.node'));
+    const nodeTitle = node.text();
+    const nodeHref = node.attr('href');
+    const nodeName = getNodeName(nodeHref);
+
+    const author = topicInfo.children(':nth-child(3)').text();
+    const createdAtOrigin = topicInfo.children(':nth-child(4)').attr('title');
+    const createdAt =
+      createdAtOrigin?.slice(0, createdAtOrigin.lastIndexOf(' ')) || '';
+    const lastRepliedBy = topicInfo.children(':nth-child(5)').text() || '';
+
+    topicList.push({
+      id: topicId,
+      title,
+      replyCount: parseInt(replyCount, 10),
+      avatar,
+      vote: parseInt(vote, 10),
+      nodeName,
+      nodeTitle,
+      author,
+      createdAt,
+      lastRepliedBy,
+    });
+  });
+
+  return topicList;
 };
--- src/services/crawler/topic.ts
+++ src/services/crawler/topic.ts
@@ -11,44 +11,7 @@ export const fetchTopicByTab = async (
   const $ = cheerio.load(response.data);
   const list = $('.cell.item');
 
-  const topicList = [] as Array<ITopic>;
-
-  list.each((_, elem) => {
-    const selector = $(elem);
-
-    const title = selector.find('.topic-link').text();
-    const titleHref = selector.find('.topic-link').attr('href');
-    const topicId = parser.getTopicId(titleHref);
-
-    const replyCount = selector.find('.count_livid').text() || '0';
-    const avatar = selector.find('.avatar').attr('src') || '';
-    const topicInfo = selector.find('.topic_info');
-    const vote = $(topicInfo.find('.votes')).text() || '0';
-
-    const node = $(topicInfo.find('.node'));
-    const nodeTitle = node.text();
-    const nodeHref = node.attr('href');
-    const nodeName = parser.getNodeName(nodeHref);
-
-    const author = topicInfo.children(':nth-child(3)').text();
-    const createdAtOrigin = topicInfo.children(':nth-child(4)').attr('title');
-    const createdAt =
-      createdAtOrigin?.slice(0, createdAtOrigin.lastIndexOf(' ')) || '';
-    const lastRepliedBy = topicInfo.children(':nth-child(5)').text() || '';
-
-    topicList.push({
-      id: topicId,
-      title,
-      replyCount: parseInt(replyCount, 10),
-      avatar,
-      vote: parseInt(vote, 10),
-      nodeName,
-      nodeTitle,
-      author,
-      createdAt,
-      lastRepliedBy,
-    });
-  });
+  const topicList = parser.parseTopicList($, list);
 
   return topicList;
 };
