391335000375fdb83f19553a97cc1e0db3963de6
xyxc0673 <xyxc0673@gmail.com>
2021-03-23 20:13:32 +0800

    feat: add Loading Component

---
+++ src/components/loading/index.tsx
@@ -0,0 +1,17 @@
+import { Colors } from '@/theme/colors';
+import React from 'react';
+import { ActivityIndicator, View } from 'react-native';
+
+interface IProps {
+  visible: boolean;
+}
+
+const LoadingComponent = ({ visible }: IProps) => {
+  return (
+    <View>{visible && <ActivityIndicator color={Colors.vi} size={48} />}</View>
+  );
+};
+
+const Loading = React.memo(LoadingComponent);
+
+export default Loading;
