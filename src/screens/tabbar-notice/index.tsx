205ff5ce0db2bf10fe990add638c0d8174550353
xyxc0673 <xyxc0673@gmail.com>
2021-03-24 20:21:37 +0800

    feat: handle notification onEndReached

--- src/screens/tabbar-notice/index.tsx
+++ src/screens/tabbar-notice/index.tsx
@@ -5,7 +5,7 @@ import { Colors } from '@/theme/colors';
 import Layout from '@/theme/layout';
 import { useAppDispatch, useAppSelector } from '@/utils/hooks';
 import React, { useEffect } from 'react';
-import { View, FlatList, StyleSheet, RefreshControl } from 'react-native';
+import { View, FlatList, StyleSheet, RefreshControl, Text } from 'react-native';
 import Notification from './components/notification';
 
 const TabbarNotice = () => {
@@ -15,6 +15,10 @@ const TabbarNotice = () => {
   const isRefreshing = useAppSelector(
     (state) => state.notification.isRefreshing,
   );
+  const maxPage = useAppSelector((state) => state.notification.maxPage);
+  const currentPage = useAppSelector((state) => state.notification.currentPage);
+
+  const noMore = maxPage === currentPage;
 
   useEffect(() => {
     dispatch(fetchUserNotifications({ refresh: false, isFirstFetching: true }));
@@ -27,6 +31,14 @@ const TabbarNotice = () => {
       </View>
     );
   }, [pending]);
+
+  const ListFooterComponent = React.useMemo(() => {
+    return (
+      <View style={styles.listFooter}>
+        <Text>{noMore ? '没有更多的消息了' : '加载中'}</Text>
+      </View>
+    );
+  }, [noMore]);
 
   return (
     <View style={Layout.fill}>
@@ -47,6 +59,11 @@ const TabbarNotice = () => {
           />
         }
         ListEmptyComponent={() => listEmptyComponent}
+        onEndReached={() =>
+          dispatch(fetchUserNotifications({ refresh: false }))
+        }
+        onEndReachedThreshold={noMore ? null : 0.1}
+        ListFooterComponent={() => ListFooterComponent}
       />
     </View>
   );
@@ -62,5 +79,10 @@ const styles = StyleSheet.create({
   },
   loading: {
     marginTop: '50%',
+  },
+  listFooter: {
+    marginTop: 8,
+    marginBottom: 16,
+    alignItems: 'center',
   },
 });
